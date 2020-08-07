99ce864a25907b80bbc044fbb8f3314c08917ae6
[Vladislav Katkowski] <[vladkarkowski@gmail.com]>
2020-08-07 10:00:28 +0300

    first commit

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
+"use strict";
 
 
-//Четвертое задание
-
-// let firstName = prompt("What is your first name?");
-// alert(`Your Name ${firstName} `);
-// let secondName = prompt("What is your second name?");
-// alert(`Your Name ${secondName} `);
-// let patronymic = prompt("What is your patronymic?");
-// alert(`Your Name ${patronymic} `);
-// let age = prompt("How old are you?");
-// alert(`Your Name ${age} `);
-// let sex = confirm("Your sex is a man?");
-
-
-
-let firstNames = prompt("What is your Full name?");
+let firstName = prompt("What is your first name?");
+let secondName = prompt("What is your second name?");
+let patronymic = prompt("What is your patronymic?");
 
 let ages = prompt('Your age in years?');
 
@@ -24,11 +13,11 @@ let ages = prompt('Your age in years?');
 // let d = future.diff(start, 'days'); 
 // alert(` ${d} days`);
 
-
 let sex = prompt("Your sex? Man/Woman");
 
 if (ages > 65 && sex === "Man") {
-  alert(`Your Full Name: ${firstNames},
+alert(`
+  Your Full Name ${firstName} ${secondName} ${patronymic},
   Your: ${ages} age!,
   Your age in: ${ages * 365} days
   Your Sex: ${sex}, 
@@ -36,7 +25,7 @@ if (ages > 65 && sex === "Man") {
   Your can rest`);
 } else if (ages > 55 && sex === "Woman") {
   alert(`
-  Your Full Name: ${firstNames},
+  Your Full Name ${firstName} ${secondName} ${patronymic},
   Your: ${ages} age!,
   Your age in: ${ages * 365} days
   Your Sex: ${sex}, 
@@ -44,7 +33,7 @@ if (ages > 65 && sex === "Man") {
   Your can rest`);
 } else {
   alert(`
-  Your Full Name: ${firstNames},
+  Your Full Name ${firstName} ${secondName} ${patronymic},
   Your: ${ages} age!,
   Your age in: ${ages * 365} days
   Your Sex: ${sex}, 
